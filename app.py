fac65232c3988ddf0348fa13052827bea4c03590
Kevin Hung <kevinhung@C02G11.local>
2016-03-13 18:49:03 -0500

    Varchar 225 update. Login completed.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, redirect, session
 from flask.ext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
 mysql = MySQL()
 app = Flask(__name__)
+app.secret_key = 'why would I tell you my secret key?'
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -20,6 +21,60 @@ def main():
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
+@app.route('/showSignin')
+def showSignin():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('signin.html')
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+ 
+ 
+ 
+        # connect to mysql
+ 
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        data = cursor.fetchall()
+ 
+ 
+ 
+ 
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+ 
+ 
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
 
 
 @app.route('/signUp',methods=['POST','GET'])
--- db_sp creation.sql
+++ db_sp creation.sql
@@ -1,18 +1,19 @@
-drop dabase BucketList;
-create dabase BucketList;
+##Part 1 Begins Below:
+drop database BucketList;
+create database BucketList;
 use BucketList;
 CREATE TABLE `BucketList`.`tbl_user` (
   `user_id` BIGINT NOT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(45) NULL,
-  `user_username` VARCHAR(45) NULL,
-  `user_password` VARCHAR(45) NULL,
+  `user_name` VARCHAR(225) NULL,
+  `user_username` VARCHAR(225) NULL,
+  `user_password` VARCHAR(225) NULL,
   PRIMARY KEY (`user_id`));
 
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createUser`(
-    IN p_name VARCHAR(45),
-    IN p_username VARCHAR(45),
-    IN p_password VARCHAR(45)
+    IN p_name VARCHAR(225),
+    IN p_username VARCHAR(225),
+    IN p_password VARCHAR(225)
 )
 BEGIN
     if ( select exists (select 1 from tbl_user where user_username = p_username) ) THEN
@@ -37,3 +38,18 @@ BEGIN
     END IF;
 END$$
 DELIMITER ;
+
+##Part 1 Ends.
+
+##Part 2 Begins Below:
+##get user details based on username using sp_validateLogin.
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validateLogin`(
+IN p_username VARCHAR(20)
+)
+BEGIN
+    select * from tbl_user where user_username = p_username;
+END$$
+DELIMITER ;
+
+##Part 2 Ends.

7ec38d212b8f36f3db04e6ee41fc4f956f0a3219
Kevin Hung <kevinhung@C02K4L.local>
2016-04-07 02:23:27 -0500

    creation of various FST commands and files

---
+++ FST.bash
@@ -0,0 +1,711 @@
+
+mkdir 'angry';
+cd angry;
+
+
+pwd;
+mkdir 'irritated';
+mkdir 'enraged';
+mkdir 'hostile';
+mkdir 'insulting';
+mkdir 'annoyed';
+mkdir 'upset';
+mkdir 'hateful';
+mkdir 'unpleasant';
+mkdir 'offensive';
+mkdir 'aggressive';
+mkdir 'bitter';
+mkdir 'frustrated';
+mkdir 'controlling';
+mkdir 'resentful';
+mkdir 'hotheaded';
+mkdir 'malicious';
+mkdir 'infuriated';
+mkdir 'critical';
+mkdir 'meanspirited';
+mkdir 'violent';
+mkdir 'vindictive';
+mkdir 'sadistic';
+mkdir 'mean';
+mkdir 'spiteful';
+mkdir 'furious';
+mkdir 'agitated';
+mkdir 'antagonistic';
+mkdir 'repulsed';
+mkdir 'mad';
+mkdir 'cross';
+mkdir 'incensed';
+mkdir 'loud';
+mkdir 'reactive';
+mkdir 'swearing';
+mkdir 'abrupt';
+mkdir 'quarrelsome';
+mkdir 'venomous';
+mkdir 'irate';
+mkdir 'shorttempered';
+mkdir 'stubborn';
+mkdir 'rebellious';
+mkdir 'exasperated';
+mkdir 'impatient';
+mkdir 'contrary';
+mkdir 'condemning';
+mkdir 'seething';
+mkdir 'scornful';
+mkdir 'sarcastic';
+mkdir 'overbearing';
+mkdir 'sharp';
+mkdir 'poisonous';
+mkdir 'disrespectful';
+mkdir 'jealous';
+mkdir 'ticked off';
+mkdir 'hitting';
+mkdir 'yelling';
+mkdir 'screaming';
+mkdir 'revengeful';
+mkdir 'retaliating';
+mkdir 'reprimanding';
+mkdir 'envious';
+cd ..;
+
+
+
+mkdir 'depressed';
+cd depressed;
+
+
+pwd;
+mkdir 'disappointed';
+mkdir 'discouraged';
+mkdir 'ashamed';
+mkdir 'powerless';
+mkdir 'diminished';
+mkdir 'guilty';
+mkdir 'dissatisfied';
+mkdir 'miserable';
+mkdir 'despicable';
+mkdir 'selfdenigrating';
+mkdir 'self-hating';
+mkdir 'sulky';
+mkdir 'low';
+mkdir 'terrible';
+mkdir 'lousy';
+mkdir 'desperate';
+mkdir 'alienated';
+mkdir 'bad';
+mkdir 'pessimistic';
+mkdir 'dejected';
+mkdir 'bummed out';
+mkdir 'self-critical';
+mkdir 'selfdeprecating';
+mkdir 'gloomy';
+mkdir 'glum';
+mkdir 'disheartened';
+mkdir 'down';
+mkdir 'despondent';
+mkdir 'cheerless';
+mkdir 'rotten';
+mkdir 'masochistic';
+mkdir 'stuck';
+mkdir 'contracted';
+mkdir 'tight';
+mkdir 'blocked';
+mkdir 'despairing';
+mkdir 'hopeless';
+mkdir 'grouchy';
+mkdir 'off';
+mkdir 'moody';
+mkdir 'crabby';
+mkdir 'faultfinding';
+mkdir 'resistant';
+mkdir 'punishing';
+mkdir 'morose';
+mkdir 'cranky';
+mkdir 'grumpy';
+mkdir 'burdened';
+mkdir 'negative';
+mkdir 'closed';
+mkdir 'out of sorts';
+mkdir 'no energy';
+mkdir 'in hell';
+mkdir 'touchy';
+mkdir 'haggard';
+mkdir 'drawn';
+mkdir 'slumped';
+mkdir 'slouching';
+mkdir 'achy';
+cd ..;
+
+
+
+mkdir 'confused';
+cd confused;
+
+
+pwd;
+mkdir 'doubtful';
+mkdir 'uncertain';
+mkdir 'indecisive';
+mkdir 'perplexed';
+mkdir 'embarrassed';
+mkdir 'hesitant';
+mkdir 'shy';
+mkdir 'disillusioned';
+mkdir 'distrustful';
+mkdir 'misgiving';
+mkdir 'lost';
+mkdir 'unsure';
+mkdir 'uneasy';
+mkdir 'tense';
+mkdir 'stressed';
+mkdir 'uncomfortable';
+mkdir 'comparing';
+mkdir 'dishonest';
+mkdir 'superior';
+mkdir 'disdainful';
+mkdir 'manipulative';
+mkdir 'judgmental';
+mkdir 'argumentative';
+mkdir 'authoritative';
+mkdir 'condescending';
+mkdir 'demanding';
+mkdir 'confounded';
+mkdir 'distracted';
+mkdir 'disoriented';
+mkdir 'off-kilter';
+mkdir 'frenzied';
+mkdir 'blushing';
+mkdir 'awkward';
+cd ..;
+
+
+
+mkdir 'helpless';
+cd helpless;
+
+
+pwd;
+mkdir 'incapable';
+mkdir 'alone';
+mkdir 'paralyzed';
+mkdir 'fatigued';
+mkdir 'useless';
+mkdir 'inferior';
+mkdir 'vulnerable';
+mkdir 'empty';
+mkdir 'distressed';
+mkdir 'pathetic';
+mkdir 'distraught';
+mkdir 'doomed';
+mkdir 'overwhelmed';
+mkdir 'incompetent';
+mkdir 'inept';
+mkdir 'incapacitated';
+mkdir 'shut down';
+mkdir 'cut off';
+mkdir 'trapped';
+mkdir 'weak';
+mkdir 'sick';
+mkdir 'nauseated';
+mkdir 'fidgety';
+mkdir 'trembling';
+mkdir 'craving';
+mkdir 'hungry';
+mkdir 'squirming';
+mkdir 'jittery';
+mkdir 'woozy';
+mkdir 'twitching';
+mkdir 'compulsive';
+cd ..;
+
+
+
+mkdir 'indifferent';
+cd indifferent;
+
+
+pwd;
+mkdir 'insensitive';
+mkdir 'dull';
+mkdir 'reserved';
+mkdir 'weary';
+mkdir 'bored';
+mkdir 'preoccupied';
+mkdir 'cold';
+mkdir 'lifeless';
+mkdir 'uncaring';
+mkdir 'uninterested';
+mkdir 'unresponsive';
+mkdir 'tired';
+mkdir 'robotic';
+mkdir 'slow';
+mkdir 'sluggish';
+cd ..;
+
+
+
+mkdir 'afraid';
+cd afraid;
+
+
+pwd;
+mkdir 'fearful';
+mkdir 'terrified';
+mkdir 'suspicious';
+mkdir 'anxious';
+mkdir 'alarmed';
+mkdir 'panicked';
+mkdir 'nervous';
+mkdir 'scared';
+mkdir 'worried';
+mkdir 'frightened';
+mkdir 'timid';
+mkdir 'shaky';
+mkdir 'restless';
+mkdir 'threatened';
+mkdir 'cowardly';
+mkdir 'insecure';
+mkdir 'wary';
+mkdir 'menaced';
+mkdir 'uptight';
+mkdir 'apprehensive';
+mkdir 'attacking';
+mkdir 'defensive';
+mkdir 'dreadful';
+mkdir 'guarded';
+mkdir 'troubled';
+mkdir 'selfabsorbed';
+mkdir 'rigid';
+mkdir 'phobic';
+mkdir 'intolerant';
+mkdir 'disturbed';
+mkdir 'disrupted';
+mkdir 'intimidated';
+mkdir 'avoiding';
+mkdir 'unwelcoming';
+mkdir 'petrified';
+mkdir 'unbending';
+mkdir 'paranoid';
+mkdir 'inhibited';
+mkdir 'immobile';
+mkdir 'attached';
+mkdir 'prejudiced';
+mkdir 'agoraphobic';
+mkdir 'selfconscious';
+cd ..;
+
+
+
+mkdir 'hurt';
+cd hurt;
+
+
+pwd;
+mkdir 'crushed';
+mkdir 'tormented';
+mkdir 'deprived';
+mkdir 'pained';
+mkdir 'tortured';
+mkdir 'rejected';
+mkdir 'injured';
+mkdir 'offended';
+mkdir 'afflicted';
+mkdir 'aching';
+mkdir 'victimized';
+mkdir 'heartbroken';
+mkdir 'agonized';
+mkdir 'appalled';
+mkdir 'wronged';
+mkdir 'humiliated';
+mkdir 'insulted';
+mkdir 'withdrawn';
+mkdir 'miffed';
+mkdir 'indignant';
+mkdir 'suffering';
+mkdir 'distant';
+mkdir 'invaded';
+mkdir 'bulldozed';
+mkdir 'bullied';
+mkdir 'secretive';
+mkdir 'slighted';
+mkdir 'smothered';
+mkdir 'belittled';
+cd ..;
+
+
+
+mkdir 'sad';
+cd sad;
+
+
+pwd;
+mkdir 'tearful';
+mkdir 'sorrowful';
+mkdir 'anguished';
+mkdir 'desolate';
+mkdir 'unhappy';
+mkdir 'lonely';
+mkdir 'grieved';
+mkdir 'mournful';
+mkdir 'dismayed';
+mkdir 'downhearted';
+mkdir 'oversensitive';
+mkdir 'dirty';
+mkdir 'remorseful';
+mkdir 'sullen';
+mkdir 'sour';
+mkdir 'selfcastigating';
+mkdir 'unworthy';
+mkdir 'fragile';
+mkdir 'disconnected';
+mkdir 'devastated';
+mkdir 'blindsided';
+mkdir 'discontented';
+mkdir 'crying';
+mkdir 'groaning';
+mkdir 'moaning';
+cd ..;
+
+
+
+mkdir 'judgemental';
+cd judgemental;
+
+
+pwd;
+mkdir 'stony';
+mkdir 'serious';
+mkdir 'stern';
+mkdir 'frowning';
+mkdir 'recoiling';
+mkdir 'glaring';
+mkdir 'disgusted';
+mkdir 'unfair';
+mkdir 'attacked';
+mkdir 'tactless';
+mkdir 'hurtful';
+mkdir 'bossy';
+mkdir 'stilted';
+mkdir 'stiff';
+mkdir 'brutal';
+mkdir 'combative';
+mkdir 'pushy';
+mkdir 'neglectful';
+mkdir 'stonewalling';
+mkdir 'rude';
+mkdir 'ranting';
+mkdir 'scolding';
+mkdir 'shrill';
+mkdir 'hard';
+mkdir 'fake';
+mkdir 'phony';
+mkdir 'shallow';
+mkdir 'taut';
+mkdir 'territorial';
+mkdir 'complaining';
+mkdir 'obsessive';
+mkdir 'blunt';
+cd ..;
+
+
+
+mkdir 'open';
+cd open;
+pwd;
+mkdir 'understanding';
+mkdir 'confident';
+mkdir 'easy';
+mkdir 'connected';
+mkdir 'free';
+mkdir 'sympathetic';
+mkdir 'interested';
+mkdir 'satisfied';
+mkdir 'receptive';
+mkdir 'accepting';
+mkdir 'kind';
+mkdir 'harmonious';
+mkdir 'empathetic';
+mkdir 'tolerant';
+mkdir 'friendly';
+mkdir 'approachable';
+mkdir 'outgoing';
+mkdir 'flowing';
+mkdir 'flexible';
+mkdir 'present';
+mkdir 'listening';
+mkdir 'welcoming';
+mkdir 'embracing';
+cd ..;
+
+
+
+mkdir 'loving';
+cd loving;
+
+
+pwd;
+mkdir 'considerate';
+mkdir 'affectionate';
+mkdir 'sensitive';
+mkdir 'tender';
+mkdir 'devoted';
+mkdir 'attracted';
+mkdir 'passionate';
+mkdir 'admiring';
+mkdir 'warm';
+mkdir 'touched';
+mkdir 'close';
+mkdir 'loved';
+mkdir 'sweet';
+mkdir 'gentle';
+mkdir 'compassionate';
+mkdir 'caring';
+mkdir 'allowing';
+mkdir 'nonjudgmental';
+mkdir 'appreciative';
+mkdir 'respectful';
+mkdir 'humble';
+mkdir 'gracious';
+mkdir 'patient';
+mkdir 'honoring';
+mkdir 'expansive';
+mkdir 'kindly';
+mkdir 'grateful';
+cd ..;
+
+
+
+mkdir 'happy';
+cd happy;
+
+
+pwd;
+mkdir 'blissful';
+mkdir 'joyous';
+mkdir 'delighted';
+mkdir 'overjoyed';
+mkdir 'gleeful';
+mkdir 'thankful';
+mkdir 'festive';
+mkdir 'ecstatic';
+mkdir 'satisfied';
+mkdir 'glad';
+mkdir 'cheerful';
+mkdir 'sunny';
+mkdir 'elated';
+mkdir 'jubilant';
+mkdir 'jovial';
+mkdir 'fun-loving';
+mkdir 'lighthearted';
+mkdir 'easygoing';
+mkdir 'mellow';
+mkdir 'happy-go-lucky';
+mkdir 'glorious';
+mkdir 'innocent';
+mkdir 'child-like';
+mkdir 'gratified';
+mkdir 'euphoric';
+mkdir 'rapturous';
+mkdir 'in good humor';
+mkdir 'in heaven';
+mkdir 'on top of the world';
+cd ..;
+
+
+
+mkdir 'interested';
+cd interested;
+
+
+pwd;
+mkdir 'fascinated';
+mkdir 'intrigued';
+mkdir 'absorbed';
+mkdir 'inquisitive';
+mkdir 'engrossed';
+mkdir 'curious';
+mkdir 'amazed';
+mkdir 'involved';
+mkdir 'attentive';
+mkdir 'observant';
+mkdir 'amused';
+mkdir 'thoughtful';
+mkdir 'courteous';
+mkdir 'intent';
+mkdir 'focused';
+
+cd ..;
+
+
+
+mkdir 'alive';
+cd alive;
+
+
+pwd;
+mkdir 'playful';
+mkdir 'courageous';
+mkdir 'energetic';
+mkdir 'liberated';
+mkdir 'optimistic';
+mkdir 'frisky';
+mkdir 'animated';
+mkdir 'spirited';
+mkdir 'thrilled';
+mkdir 'wonderful';
+mkdir 'funny';
+mkdir 'great';
+mkdir 'giving';
+mkdir 'sharing';
+mkdir 'intelligent';
+mkdir 'exhilarated';
+mkdir 'equal';
+mkdir 'excited';
+mkdir 'enjoying';
+mkdir 'communicative';
+mkdir 'active';
+mkdir 'spunky';
+mkdir 'youthful';
+mkdir 'vigorous';
+mkdir 'tickled';
+cd ..;
+
+
+
+mkdir 'positive';
+cd positive;
+
+
+pwd;
+mkdir 'eager';
+mkdir 'keen';
+mkdir 'earnest';
+mkdir 'inspired';
+mkdir 'enthusiastic';
+mkdir 'bold';
+mkdir 'brave';
+mkdir 'daring';
+mkdir 'hopeful';
+mkdir 'upbeat';
+mkdir 'beautiful';
+mkdir 'creative';
+mkdir 'constructive';
+mkdir 'helpful';
+mkdir 'resourceful';
+mkdir 'motivated';
+mkdir 'cooperative';
+mkdir 'productive';
+mkdir 'exuberant';
+mkdir 'in the zone';
+mkdir 'responsive';
+mkdir 'conscientious';
+mkdir 'approving';
+mkdir 'honored';
+mkdir 'privileged';
+mkdir 'adaptable';
+cd ..;
+
+
+
+mkdir 'peaceful';
+cd peaceful;
+
+
+pwd;
+mkdir 'calm';
+mkdir 'good';
+mkdir 'at ease';
+mkdir 'comfortable';
+mkdir 'pleased';
+mkdir 'encouraged';
+mkdir 'surprised';
+mkdir 'content';
+mkdir 'quiet';
+mkdir 'certain';
+mkdir 'relaxed';
+mkdir 'serene';
+mkdir 'bright';
+mkdir 'blessed';
+mkdir 'assured';
+mkdir 'clear';
+mkdir 'balanced';
+mkdir 'fine';
+mkdir 'okay';
+mkdir 'grateful';
+mkdir 'carefree';
+mkdir 'adequate';
+mkdir 'fulfilled';
+mkdir 'genuine';
+mkdir 'authentic';
+mkdir 'forgiving';
+mkdir 'sincere';
+mkdir 'uplifted';
+mkdir 'unburdened';
+mkdir 'confident';
+mkdir 'self-sufficient';
+cd ..;
+
+
+
+mkdir 'strong';
+cd strong;
+
+
+pwd;
+mkdir 'reliable';
+mkdir 'sure';
+mkdir 'unique';
+mkdir 'dynamic';
+mkdir 'tenacious';
+mkdir 'hardy';
+mkdir 'secure';
+mkdir 'stable';
+mkdir 'honest';
+mkdir 'composed';
+mkdir 'self-affirming';
+mkdir 'truthful';
+mkdir 'supportive';
+mkdir 'excellent';
+mkdir 'perseverant';
+mkdir 'responsible';
+mkdir 'energized';
+mkdir 'sane';
+mkdir 'complete';
+mkdir 'mature';
+mkdir 'solid';
+cd ..;
+
+
+
+mkdir 'relaxed';
+cd relaxed;
+
+
+pwd;
+mkdir 'glowing';
+mkdir 'radiant';
+mkdir 'beaming';
+mkdir 'reflective';
+mkdir 'smiling';
+mkdir 'grounded';
+mkdir 'unhurried';
+mkdir 'open-minded';
+mkdir 'efficient';
+mkdir 'non-controlling';
+mkdir 'unassuming';
+mkdir 'trusting';
+mkdir 'supported';
+mkdir 'fluid';
+mkdir 'light';
+mkdir 'spontaneous';
+mkdir 'aware';
+mkdir 'healthy';
+mkdir 'meditative';
+mkdir 'still';
+mkdir 'rested';
+mkdir 'waiting';
+mkdir 'laughing';
+mkdir 'graceful';
+mkdir 'natural';
+mkdir 'steady';
+mkdir 'centered';
+mkdir 'placid';

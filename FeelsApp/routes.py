81098e533eb7984cd885d573a2ff3ce678ed49f8
k-hung <khung@luc.edu>
2016-05-01 17:09:55 -0500

    .jpg update

--- FeelsApp/routes.py
+++ FeelsApp/routes.py
@@ -10,7 +10,7 @@ mail = Mail()
 @app.route('/')
 def home():
   return render_template('home.html')
-  
+
 @app.route('/about')
 def about():
   return render_template('about.html')
@@ -18,7 +18,7 @@ def about():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
   form = ContactForm()
- 
+
   if request.method == 'POST':
     if form.validate() == False:
       flash('All fields are required.')
@@ -30,19 +30,19 @@ def contact():
       %s
       """ % (form.name.data, form.email.data, form.message.data)
       mail.send(msg)
- 
+
       return render_template('contact.html', success=True)
- 
+
   elif request.method == 'GET':
     return render_template('contact.html', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
   form = SignupForm()
- 
+
   if 'email' in session:
-    return redirect(url_for('profile')) 
-   
+    return redirect(url_for('profile'))
+
   if request.method == 'POST':
     if form.validate() == False:
       return render_template('signup.html', form=form)
@@ -50,38 +50,38 @@ def signup():
       newuser = User(form.firstname.data, form.lastname.data, form.email.data, form.password.data)
       db.session.add(newuser)
       db.session.commit()
-       
+
       session['email'] = newuser.email
       return redirect(url_for('profile'))
-   
+
   elif request.method == 'GET':
     return render_template('signup.html', form=form)
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
   form = SigninForm()
- 
+
   if 'email' in session:
-    return redirect(url_for('profile'))  
-   
+    return redirect(url_for('profile'))
+
   if request.method == 'POST':
     if form.validate() == False:
       return render_template('signin.html', form=form)
     else:
       session['email'] = form.email.data
       return redirect(url_for('profile'))
-                 
+
   elif request.method == 'GET':
     return render_template('signin.html', form=form)
 
 @app.route('/profile')
 def profile():
- 
+
   if 'email' not in session:
     return redirect(url_for('signin'))
- 
+
   user = User.query.filter_by(email = session['email']).first()
- 
+
   if user is None:
     return redirect(url_for('signin'))
   else:
@@ -89,10 +89,10 @@ def profile():
 
 @app.route('/signout')
 def signout():
- 
+
   if 'email' not in session:
     return redirect(url_for('signin'))
-     
+
   session.pop('email', None)
   return redirect(url_for('home'))
 
